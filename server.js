537ff9bc5a030db074e5a240695fb2ce141be7d4
ab77441226-cpu <ab77441226@gmail.com>
2026-09-26 10:39:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const bcrypt = require("bcryptjs");
 const path = require("path");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 /* =========================================================
    MIDDLEWARE
